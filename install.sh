aae014865811015e18c40a1ce3e04920ecc3d100
Myagdeev Vladimir <concertbil@gmail.com>
2017-03-29 22:05:38 +0500

    Installer for Ansible+Apache+MySQL+PHP7

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt-get install libssl-dev
+sudo apt-get install -y python-dev python-pip
+sudo -H pip install ansible
+sudo ansible-galaxy install geerlingguy.apache geerlingguy.mysql geerlingguy.php
+sudo ansible-paybook -i 'localhost,' -c local lamp.yml
