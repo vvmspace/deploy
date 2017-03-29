6c23aafdae3b3110c30beca46ce5fc5c86c160b5
Myagdeev Vladimir <concertbil@gmail.com>
2017-03-29 23:46:54 +0500

    Fix

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo apt-get install libssl-dev
 sudo apt-get install -y python-dev python-pip
 sudo -H pip install ansible
 sudo ansible-galaxy install geerlingguy.apache geerlingguy.mysql geerlingguy.php
-sudo ansible-paybook -i 'localhost,' -c local lamp.yml
+sudo ansible-playbook -i 'localhost,' -c local lamp.yml
 chmod +x ./getcomposer.sh
 sudo sh ./getcomposer.sh
