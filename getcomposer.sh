6b3605cd9dccb851e858162af045041c37332d21
Myagdeev Vladimir <concertbil@gmail.com>
2017-03-29 22:08:21 +0500

    Added composer installer

---
+++ getcomposer.sh
@@ -0,0 +1,3 @@
+curl -s http://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/
+alias composer='/usr/local/bin/composer.phar'
--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt-get install -y python-dev python-pip
 sudo -H pip install ansible
 sudo ansible-galaxy install geerlingguy.apache geerlingguy.mysql geerlingguy.php
 sudo ansible-paybook -i 'localhost,' -c local lamp.yml
+chmod +x ./getcomposer.sh
+sudo sh ./getcomposer.sh
