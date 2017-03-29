0973bb2c8d03ee991aba0463e5f77b0e0f210560
Myagdeev Vladimir <concertbil@gmail.com>
2017-03-29 21:59:48 +0500

    Revert "Apache+MySQL+PHP7+Ansible installer"
    This reverts commit f96f0c086612c97e1af15a7b8fbd2124454760ca.

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt-get install libssl-dev
-sudo apt-get install -y python-dev python-pip
-sudo -H pip install ansible
-sudo ansible-galaxy install geerlingguy.apache geerlingguy.mysql geerlingguy.php
-wget https://gist.githubusercontent.com/popstas/61a5ddce08..
-sudo ansible-paybook -i 'localhost,' -c local lamp.yml
